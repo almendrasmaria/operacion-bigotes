28f5170212bf91fa236b99de3ba52be34db77f52
maria <almendrasmaria04@gmail.com>
2025-09-16 01:46:37 -0300

    Added initial cat data in cats.js

---
+++ src/data/cats.js
@@ -0,0 +1,12 @@
+export const cats = [
+  {
+    id: "c1",
+    name: "Copito",
+    sex: "male", 
+    ageYears: 3,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    shelterHandle: "patitaslugano",
+    location: "Villa Lugano, CABA",
+    photo: "./images/cats/copito.jpg" 
+  }
+];
