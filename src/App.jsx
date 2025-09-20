d22b1287a18588c42b6cb37b24ad7d4c43f3dd0b
maria <almendrasmaria04@gmail.com>
2025-09-20 00:47:05 -0300

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero/>
     </>
   );
 }
